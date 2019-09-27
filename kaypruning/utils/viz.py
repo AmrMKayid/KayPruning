28a735b7ecc0b8c318cfe15adb1a221665f19bf6
amrmkayid <amrkayid2027@gmail.com>
2019-09-27 03:59:47 +0200

    Update viz

--- kaypruning/utils/viz.py
+++ kaypruning/utils/viz.py
@@ -1,9 +1,7 @@
 import matplotlib.pyplot as plt
 
-from kaypruning.configs import prune_configs
 
-
-def plot(x=prune_configs.k, y=0, prune_type='weight', type='Accuracy'):
+def plot(x=[0, 25, 50, 60, 70, 80, 90, 95, 97, 99], y=0, prune_type='weight', type='Accuracy'):
     plt.plot(x, y)
     plt.title(f'{prune_type} Pruning')
     plt.xlabel('Sparsity (%)')
